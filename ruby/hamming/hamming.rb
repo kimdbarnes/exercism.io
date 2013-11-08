47e96ac720314cf59eb5d30cdd38cb9f4624df29
kimdbarnes <kimdbarnes@gmail.com>
2013-11-07 20:26:40 -0700

    Third try

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,19 @@
 class Hamming
-  def self.compute(first, second)
-    differences = 0
-    shorter = first.length < second.length ? first : second
-    longer = shorter == first ? second : first
-    (0..shorter.length-1).each do |index|
-      differences += 1 if shorter[index] != longer[index]
+  class << self
+
+    def compute(first, second)
+      shorter = _shorter(first, second)
+      longer = _longer(first, second)
+
+      (0..shorter.length-1).count { |index| shorter[index] != longer[index] }
     end
-    differences
+
+    def _shorter(first, second)
+      first.length < second.length ? first : second
+    end
+
+    def _longer(first, second)
+      first.length < second.length ? second : first
+    end
   end
 end
