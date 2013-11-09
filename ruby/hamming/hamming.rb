29dbd25d9498f683cf5d08c09f90b29ebe780bff
Kim D Barnes <kimdbarnes@gmail.com>
2013-11-09 08:11:57 -0700

    Katrina inspired 1-liner

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,18 +2,7 @@ class Hamming
   class << self
 
     def compute(first, second)
-      shorter = _shorter(first, second)
-      longer = _longer(first, second)
-
-      (0..shorter.length-1).count { |index| shorter[index] != longer[index] }
-    end
-
-    def _shorter(first, second)
-      first.length < second.length ? first : second
-    end
-
-    def _longer(first, second)
-      first.length < second.length ? second : first
+      (0..[first.length, second.length].min-1).count { |i| first[i] != second[i] }
     end
   end
 end
