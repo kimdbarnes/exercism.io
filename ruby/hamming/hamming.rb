701a8d51dc671715a6f3a607a02b6d2127be8d59
Kim D Barnes <kimdbarnes@gmail.com>
2013-11-10 10:44:32 -0700

    Shorter by a bit

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,7 +2,7 @@ class Hamming
   class << self
 
     def compute(first, second)
-      (0..[first.length, second.length].min-1).count { |i| first[i] != second[i] }
+      (0...[first.length, second.length].min).count { |i| first[i] != second[i] }
     end
   end
 end
